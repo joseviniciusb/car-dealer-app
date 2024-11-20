7eba47dc1620598cd1af2721e153261ec2855764
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-11-20 22:48:38 +0000

    feat: make a request to the api in search of the model and year selected as a parameter

--- app/result/[make]/[year]/page.tsx
+++ app/result/[make]/[year]/page.tsx
@@ -1,23 +1,41 @@
-"use client";
-import { useState } from "react";
+async function fetchVehicleModels(make: string, year: string) {
+  const res = await fetch(
+    `https://vpic.nhtsa.dot.gov/api/vehicles/getModelsForMakeYear?make=${make}&year=${year}&format=json`
+  );
+  const data = await res.json();
+  console.log(res, "res");
 
-export default async function ResultPage() {
-  const vehicleFound = useState(true);
-  const [selectedMake, setSelectedMake] = useState("CHEVROLET");
-  const [selectedYear, setSelectedYear] = useState("2024");
+  if (!data.Results) {
+    throw new Error("No vehicle models found");
+  }
+  console.log("result:", data);
+}
 
+export default async function ResultPage({
+  params: { make: selectedMake, year: selectedYear },
+}: {
+  params: { make: string; year: string };
+}) {
+  const vehicleModels = await fetchVehicleModels(
+    selectedMake,
+    selectedYear
+  ).catch(() => []);
+
+  console.log("length", vehicleModels?.length);
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6">
       <h1 className="text-3xl font-bold mb-8">
-        {`Vehicle Models for ${selectedMake} - ${selectedYear}`}
+        Vehicle Models for {selectedMake} - {selectedYear}
       </h1>
       <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
         <h2 className="text-2xl font-semibold mb-4">Available Models:</h2>
         <ul className="space-y-2">
-          {vehicleFound ? (
-            <li key={0} className="text-lg">
-              {"model"}
-            </li>
+          {vehicleModels.length > 0 ? (
+            vehicleModels.map((model, index) => (
+              <li key={index} className="text-lg">
+                {model}
+              </li>
+            ))
           ) : (
             <li>No models available for this make and year.</li>
           )}
