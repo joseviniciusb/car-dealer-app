ba4ff4f9e447081cdd7c499966f7019e25054cb3
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-11-20 22:53:15 +0000

    refactor: remove logs, return typed results

--- app/result/[make]/[year]/page.tsx
+++ app/result/[make]/[year]/page.tsx
@@ -3,35 +3,31 @@ async function fetchVehicleModels(make: string, year: string) {
     `https://vpic.nhtsa.dot.gov/api/vehicles/getModelsForMakeYear?make=${make}&year=${year}&format=json`
   );
   const data = await res.json();
-  console.log(res, "res");
 
   if (!data.Results) {
     throw new Error("No vehicle models found");
   }
-  console.log("result:", data);
+
+  return data.Results.map((item: any) => item.ModelName);
 }
 
 export default async function ResultPage({
-  params: { make: selectedMake, year: selectedYear },
+  params: { make, year },
 }: {
   params: { make: string; year: string };
 }) {
-  const vehicleModels = await fetchVehicleModels(
-    selectedMake,
-    selectedYear
-  ).catch(() => []);
+  const vehicleModels = await fetchVehicleModels(make, year).catch(() => []);
 
-  console.log("length", vehicleModels?.length);
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6">
       <h1 className="text-3xl font-bold mb-8">
-        Vehicle Models for {selectedMake} - {selectedYear}
+        Vehicle Models for {make} - {year}
       </h1>
       <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
         <h2 className="text-2xl font-semibold mb-4">Available Models:</h2>
         <ul className="space-y-2">
           {vehicleModels.length > 0 ? (
-            vehicleModels.map((model, index) => (
+            vehicleModels.map((model: string, index: string) => (
               <li key={index} className="text-lg">
                 {model}
               </li>
