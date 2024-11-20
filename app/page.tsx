3b948859b36084ecfc0ea8368aa499b6ae976578
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-11-20 19:55:44 +0000

    ft: Implement select vehicle filter form

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,27 @@ export default function Home() {
       <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
         My car
       </div>
+
+      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6 mt-5">
+        <form className="space-y-4">
+          <div>
+            <label
+              htmlFor="make"
+              className="block text-lg font-medium text-gray-700"
+            >
+              Select Vehicle Make
+            </label>
+            <select
+              id="make"
+              className="w-full p-2 border border-gray-300 rounded-lg"
+              value={0}
+            >
+              <option value="">Select Make</option>
+              veh make
+            </select>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
