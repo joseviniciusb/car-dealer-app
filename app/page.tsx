a566d147a11f1207afbac343b2c6d1d8e7370c55
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-11-20 20:01:46 +0000

    ft: impl and stilish select of model year and initial Next button link interface

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         My car
       </div>
 
-      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6 mt-5">
+      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
         <form className="space-y-4">
           <div>
             <label
@@ -24,6 +24,24 @@ export default function Home() {
               <option value="">Select Make</option>
               veh make
             </select>
+          </div>
+
+          <div>
+            <label
+              htmlFor="year"
+              className="block text-lg font-medium text-gray-700"
+            >
+              Select Model Year
+            </label>
+            <select
+              id="year"
+              className="w-full p-2 border border-gray-300 rounded-lg"
+              value="2011"
+            ></select>
+          </div>
+
+          <div>
+            <a href={`/result/${"lala"}/${"land"}`}>Next</a>
           </div>
         </form>
       </div>
