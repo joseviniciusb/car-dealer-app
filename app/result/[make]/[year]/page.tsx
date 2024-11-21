0aac3cb28f5e849d9d09733dabca583205c7e793
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2024-11-21 17:09:19 -0300

    Revert "Jv 1.0 - Refinements"

--- app/result/[make]/[year]/page.tsx
+++ app/result/[make]/[year]/page.tsx
@@ -1,24 +1,21 @@
-import Loading from "@/app/components/Loading";
-import React, { Suspense } from "react";
+async function fetchVehicleModels(make: string, year: string) {
+  const res = await fetch(
+    `https://vpic.nhtsa.dot.gov/api/vehicles/getModelsForMakeYear?make=${make}&year=${year}&format=json`
+  );
+  const data = await res.json();
+
+  if (!data.Results) {
+    throw new Error("No vehicle models found");
+  }
+
+  return data.Results.map((item: any) => item.ModelName);
+}
 
 export default async function ResultPage({
   params: { make, year },
 }: {
   params: { make: string; year: string };
 }) {
-  const fetchVehicleModels = async (make: string, year: string) => {
-    const res = await fetch(
-      `https://vpic.nhtsa.dot.gov/api/vehicles/getModelsForMakeYear?make=${make}&year=${year}&format=json`
-    );
-    const data = await res.json();
-
-    if (!data.Results) {
-      throw new Error("No vehicle models found");
-    }
-
-    return data.Results.map((item: any) => item.ModelName);
-  };
-
   const vehicleModels = await fetchVehicleModels(make, year).catch(() => []);
 
   return (
@@ -26,25 +23,20 @@ export default async function ResultPage({
       <h1 className="text-3xl font-bold mb-8">
         Vehicle Models for {make} - {year}
       </h1>
-
-      <Suspense fallback={<Loading />}>
-        <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
-          <h2 className="text-2xl font-semibold mb-4">Available Models:</h2>
-          <ul className="space-y-2">
-            {vehicleModels.length > 0 ? (
-              vehicleModels.map(
-                (model: string, index: React.Key | null | undefined) => (
-                  <li key={index} className="text-lg">
-                    {model}
-                  </li>
-                )
-              )
-            ) : (
-              <li>No models available for this make and year.</li>
-            )}
-          </ul>
-        </div>
-      </Suspense>
+      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
+        <h2 className="text-2xl font-semibold mb-4">Available Models:</h2>
+        <ul className="space-y-2">
+          {vehicleModels.length > 0 ? (
+            vehicleModels.map((model: string, index: string) => (
+              <li key={index} className="text-lg">
+                {model}
+              </li>
+            ))
+          ) : (
+            <li>No models available for this make and year.</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
